682e7e34f616760b21b99f4ad00d817718ec0748
Rahul Verma <rv404674@gmail.com>
2019-02-19 12:00:39 +0530

    Return response in json as well as string format

--- src/main/main.go
+++ src/main/main.go
@@ -14,7 +14,22 @@ func getCats(c echo.Context) error{
 	catName := c.QueryParam("name")
 	catType := c.QueryParam("type")
 
-	return c.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\n and his type is :%s\n", catName, catType))
+	dataType := c.Param("data")
+
+	if dataType == "string"{
+		return c.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\n and his type is :%s\n", catName, catType))
+	}
+
+	if dataType == "json"{
+		return c.JSON(http.StatusOK, map[string]string{
+			"name":catName,
+			"type":catType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error" : "you need to let us know if you want json or string data",
+	})
 
 }
 
@@ -25,7 +40,7 @@ func main(){
 	e := echo.New()
 
 	e.GET("/", hello)
-	e.GET("/cats", getCats)
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":8000")
 }
